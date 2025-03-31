03c974adbe9a3cfecebb7e18d6e8b461da341e5c
Theosik <huet.theo14022005@gmail.com>
2025-03-31 15:45:41 +0200

    ...

--- Scripts/Range.js
+++ Scripts/Range.js
@@ -1,6 +1,11 @@
 const value = document.getElementById('value');
 const input = document.getElementById('range');
-value.textContent = input.value + " min";
+value.textContent = "0 min";
 input.addEventListener("input", (event) => {
-  value.textContent = event.target.value + " min";
+	if (event.target.value < 60) {
+		value.textContent = event.target.value + " min";
+	}
+	else {
+		value.textContent = Math.floor(event.target.value/60) + " h et " +  (event.target.value%60) + " min";
+	}
 });
