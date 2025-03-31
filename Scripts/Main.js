f2b8ecf189328891e9b8e4bc53d4bbf1cd8f52b3
Matthieu777-Foz <fozmoh.matt@gmail.com>
2025-03-31 16:17:00 +0200

    push modifs

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -175,3 +175,30 @@ style.textContent = `
 `;
 
 document.head.appendChild(style);
+
+function requestFullscreen() {
+    // Vérifie si le document n'est pas déjà en plein écran
+    if (!document.fullscreenElement && 
+        !document.mozFullScreenElement &&
+        !document.webkitFullscreenElement &&
+        !document.msFullscreenElement) {
+        
+        // Demande à afficher en plein écran
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        }
+    }
+}
+
+// Écoute les événements d'interaction de l'utilisateur
+document.addEventListener('click', requestFullscreen);
+document.addEventListener('keydown', requestFullscreen);
+document.addEventListener('touchstart', requestFullscreen);
+document.addEventListener('mousedown', requestFullscreen);
+
